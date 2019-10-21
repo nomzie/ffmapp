4b91fbedb66a0bbfed4556442e88f43bd05460ab
Shae <shae@Shaes-MacBook-Air.local>
2019-10-21 15:48:55 +0800

    Add build directory

---
+++ build/precache-manifest.210a789d3e4905f5df6282edeeafea47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47e6c66baddf010618724ce0b456baa9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "645d9b6fbc628afbaf74",
+    "url": "/static/css/2.d7a94725.chunk.css"
+  },
+  {
+    "revision": "1188c1fe3af2eecd6ff1",
+    "url": "/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "645d9b6fbc628afbaf74",
+    "url": "/static/js/2.8a89617e.chunk.js"
+  },
+  {
+    "revision": "1188c1fe3af2eecd6ff1",
+    "url": "/static/js/main.6ccb30c4.chunk.js"
+  },
+  {
+    "revision": "9452dcfe521373c2bf41",
+    "url": "/static/js/runtime-main.2ae3ed8a.js"
+  }
+]);
